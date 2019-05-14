1ee28f34cdb9564a9a08c2673bd4a296c54b752d
gilescarlos32 <46952491+gilescarlos32@users.noreply.github.com>
2019-05-14 14:45:15 -0700

    Add files via upload
    Big Numbers Unit Testing

---
+++ main.cpp
@@ -0,0 +1,69 @@
+/**
+ * @file main.cpp
+ * @author Giles Carlos
+ * @brief Test BigNumbers.
+ *
+ */
+
+#include <gtest/gtest.h>
+#include "BigNumbersLib.h"
+
+class BigNumbersTest : public testing::Test {
+    
+};
+
+TEST_F(BigNumbersTest, CanAdd) {
+    BigNum::BigNumbers first("10000000001");
+    BigNum::BigNumbers second("999999999");
+    
+    BigNum::BigNumbers actual = first + second;
+    
+    BigNum::BigNumbers expected("11000000000");
+    
+    ASSERT_TRUE((expected == actual));
+}
+
+TEST_F(BigNumbersTest, CanSubtract) {
+    BigNum::BigNumbers first("80000000000");
+    BigNum::BigNumbers second("55600000");
+    
+    BigNum::BigNumbers actual = first - second;
+    
+    BigNum::BigNumbers expected("79944400000");
+    
+    ASSERT_TRUE((expected == actual));
+}
+
+TEST_F(BigNumbersTest, CanMultiply) {
+    BigNum::BigNumbers first("24935130");
+    BigNum::BigNumbers second("15570");
+    
+    BigNum::BigNumbers actual = first * second;
+    
+    BigNum::BigNumbers expected("388239974100");
+    
+    ASSERT_TRUE((expected == actual));
+}
+
+TEST_F(BigNumbersTest, CanDivide) {
+    BigNum::BigNumbers first("42300000000");
+    BigNum::BigNumbers second("65789");
+    
+    BigNum::BigNumbers actual = first / second;
+    
+    BigNum::BigNumbers expected("10");
+    
+    ASSERT_TRUE((expected == actual));
+}
+
+TEST_F(BigNumbersTest, CanGetRemainder) {
+    BigNum::BigNumbers first("2470000000000");
+    BigNum::BigNumbers second("2470000000000");
+    
+    BigNum::BigNumbers actual = first % second;
+    
+    BigNum::BigNumbers expected("0");
+    
+    ASSERT_TRUE((expected == actual));
+}
+
