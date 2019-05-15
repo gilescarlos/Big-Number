89b317e6a9d60f8295eb08dac8428f42c4bce18b
gilescarlos32 <46952491+gilescarlos32@users.noreply.github.com>
2019-05-14 17:00:26 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
-#include "mainwindow.h"
-#include <QApplication>
+/**
+ * @file main.cpp
+ * @author Giles Carlos
+ * @brief Test BigNumbers.
+ *
+ */
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+#include <gtest/gtest.h>
+#include "BigNumbersLib.h"
 
-    return a.exec();
+class BigNumbersTest : public testing::Test {
+    
+};
+
+TEST_F(BigNumbersTest, CanAdd) {
+    BigNum::BigNumbers x("10000000001");
+    BigNum::BigNumbers y("999999999");
+    
+    BigNum::BigNumbers real = x + y;
+    
+    BigNum::BigNumbers expected("11000000000");
+    
+    ASSERT_TRUE((expected == real));
 }
+
+TEST_F(BigNumbersTest, CanSubtract) {
+    BigNum::BigNumbers x("80000000000");
+    BigNum::BigNumbers y("55600000");
+    
+    BigNum::BigNumbers real = x - y;
+    
+    BigNum::BigNumbers expected("79944400000");
+    
+    ASSERT_TRUE((expected == real));
+}
+
